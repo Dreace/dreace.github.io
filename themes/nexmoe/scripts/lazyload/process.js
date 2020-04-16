dbfe20f17f6b57995f93db86fedb6f3e7933036b
Dreace <Dreace@Foxmail.com>
2020-04-17 01:13:01 +0800

    使用 jsDelivr 作为图片 CDN

--- themes/nexmoe/scripts/lazyload/process.js
+++ themes/nexmoe/scripts/lazyload/process.js
@@ -2,18 +2,19 @@
 
 function lazyProcess(htmlContent) {
     let loadingImage = this.config.lazyload.loadingImg
+    let CNDUrl = this.config.imgCDN && this.config.imgCDN.enable ? this.config.imgCDN.url : '';
     return htmlContent.replace(/<img(\s*?)src="(.*?)"(.*?)>/gi, (str, beforeClassStr, srcStr, afterClassStr) => {
         if (/data-src/gi.test(str)) {
             return str;
         }
-        str = str.replace(/alt>/gi,">");
+        str = str.replace(/alt>/gi, ">");
         if (/class="(.*?)"/gi.test(str)) {
             str = str.replace(/class="(.*?)"/gi, (classStrAll, classStr) => {
                 return classStrAll.replace(classStr, `${classStr} lazyload`);
             })
-            return str.replace(`src="${srcStr}"`, `src="${loadingImage}" data-src="${srcStr}"`);
+            return str.replace(`src="${srcStr}"`, `src="${loadingImage}" data-src="${CNDUrl + srcStr}"`);
         }
-        return str.replace(`src="${srcStr}"`, `class="lazyload" src="${loadingImage}" data-src="${srcStr}"`);
+        return str.replace(`src="${srcStr}"`, `class="lazyload" src="${loadingImage}" data-src="${CNDUrl + srcStr}"`);
     });
 }
 
